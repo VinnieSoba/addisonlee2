7041c19315673867cdac896f99dca76836eef04b
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-01-28 21:50:15 +0000

    esg page styling added to new sections and esg roadmap component php file

--- wp-content/themes/addlee/templates/component-parts/component-esg-roadmap.php
+++ wp-content/themes/addlee/templates/component-parts/component-esg-roadmap.php
@@ -1,41 +1,65 @@
 <section class="roadmap--container">
 <div class="container">
+<?php while(have_rows('esg_roadmap')) : the_row(); ?>
+        <?php 
+            $select_background_image = get_sub_field('select_background_image');
+            $background_image = get_sub_field('background_image');
+            $title = get_sub_field('header_dark_title');
+        ?>
 
-<?php 
-while(have_rows('esg_roadmap')) : the_row();
-$select_background_image = get_sub_field('select_background_image');
-$background_image = get_sub_field('background_image');
-$title = get_sub_field('header_dark_title');
-?>
-     <div class="header-title">
-        <h2><?php echo $title; ?></h2>
-    </div>
+     <div class="header-title"><h2><?php echo $title; ?></h2></div>
 
-    <div class="wrapper <?php if($select_background_image == 'yes') echo 'background-active'; ?>"  style="background-image: url(<?php if($select_background_image == 'yes') echo  $background_image; ?>)">
+    <div class="wrapper <?php if($select_background_image == 'yes') echo 'background-active'; ?>"  style="background-image: url(<?php if($select_background_image == 'yes') echo  $background_image; ?>)"> 
 
-    <?php while(have_rows('fields')) : the_row();
-        $title = get_sub_field('title');
-        $image = get_sub_field('image');
-        $content = get_sub_field('content');
-    ?>
+        <div class="row">
 
-        <div class="col-xs-12 col-md-4 col-lg-4">
-        <div class="item-card">
-            <div class="item-card--image">
-                <img src="<?php echo $image; ?>"  class="esg-image-container" alt="addison lee esg" />
-            </div>
-                <div class="item-card--content">
-                <h4><?php echo $title; ?></h4>
-                    <p><?php echo $content; ?></p>
-                </div>
-            </div>
+            <?php while(have_rows('fields')) : the_row(); ?>
+                
+                <?php
+                $column_count = get_sub_field('column_count');
+
+                    if($column_count == 12) : 
+                        $div_class = 'col-xs-12 col-md-12';
+                    elseif($column_count == 6) : 
+                        $div_class = 'col-xs-12 col-md-6 col-lg-6';
+                    elseif($column_count == 3) :
+                        $div_row = 'row';
+                        $div_class = 'col-xs-12 col-md-4 col-lg-4';
+                    elseif($column_count == 4) :
+                        $div_class = 'col-xs-12 col-md-3 col-lg-3';
+                    
+                    endif;
+                    ?>
+                   
+                    
+                            <?php while(have_rows('time_line_panel') ) : the_row(); ?>
+                                <?php 
+                                        $title = get_sub_field('title');
+                                        $image = get_sub_field('images');
+                                        $content = get_sub_field('content');
+                                        $column_count = get_sub_field('column_count');
+                                ?>    
+                
+                                <!-- start card item -->
+                                <div class="item-card--wrapper <?php echo $div_class; ?>">
+                                <div class="item-card">
+                                    <div class="item-card--image">
+                                        <img src="<?php echo $image; ?>"  class="esg-image-container" alt="addison lee esg" />
+                                    </div>
+                                    <div class="item-card--content">
+                                        <h4><?php echo $title; ?></h4>
+                                        <p><?php echo $content; ?></p>
+                                    </div>
+                                </div>
+                                <!-- end card item --> 
+                                </div>
+                            <?php endwhile; ?> <!-- loop through time line panel-->
+                    
+                    
+                
+            <?php endwhile; ?> <!-- loop through fields-->
         </div>
-       
-       
-    <?php endwhile; ?>
-    
-    </div>
-
-<?php endwhile; ?>
+    </div><!-- div class wrapper -->
+<?php endwhile; ?> <!--end esg roadmap loop -->
 </div>    
 </section>
