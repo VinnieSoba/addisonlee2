2641d3ea88449159e6d20f0a0ad0e7e526657e47
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-04-02 13:20:32 +0100

    tracking link for both services international and next day input has new redirect, sidebar-couriers.php

--- wp-content/themes/addlee/sidebar-couriers.php
+++ wp-content/themes/addlee/sidebar-couriers.php
@@ -1,25 +1,28 @@
 
     <div id="parcel-form" class="container">
         <h3>Already sent a parcel?</h3>
-        <form class="parcel-tracker" name="parcel-tracker" action="https://addlee.netcourier.net/" method="get">
-            <input type="hidden" name="module" value="track">
+        <!--<form class="parcel-tracker" name="parcel-tracker" action="https://addlee.netcourier.net/" method="get">-->
+            <!--<input type="hidden" name="module" value="track">-->
             <div class="col-md-4">
                 <label for="hawb">Track your job</label>
             </div>
             <div class="col-md-4">
-                <input type="text" onfocus="if (this.value == 'AWB Number') {this.value = '';}" onblur="if (this.value == '') {this.value = 'AWB Number';}" value="AWB Number" class="text watermark" name="hawb">
+                <input type="text" id="hawb" onclick="doTrack();" onfocus="if (this.value == 'AWB Number') {this.value = '';}" onblur="if (this.value == '') {this.value = 'AWB Number';}" value="AWB Number" class="text watermark">
                 <script>
-                    jQuery(function() {
-                        jQuery(":input[name=hawb]");
-                    });
+                    function doTrack() {
+                        var hawb = document.getElementById('hawb').value;
+                        if (hawb)
+                        window.location.href =
+                        "https://addlee.netcourier.net/online/tracking/trackinginfo/"  + hawb;
+                    }
                 </script>
             </div>
             <div class="col-md-3">
-                <button name="next" class="cta inverse" type="submit">
+                <button onclick="doTrack();" class="cta inverse">
                     <span>Find</span>
                 </button>
             </div>
-        </form>
+        <!--</form>-->
     </div>
 
     <div class="textwidget">
