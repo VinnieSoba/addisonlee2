2fda2072c347c951171cb5a21aa698db4495b1cc
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-04-08 14:54:24 +0100

    deploy template-test file to staging for the first time

--- wp-content/themes/addlee/templates/template-test.php
+++ wp-content/themes/addlee/templates/template-test.php
@@ -13,6 +13,26 @@ get_header();
     </h1>
 </section>
 
+<section class="query-database">
+<?php
+//global $wpdb;
+$query = "SELECT * FROM `czbhw794wh_posts` ORDER BY `post_date` LIMIT 0,4";
+$result = $wpdb->get_results($query);
+foreach ($result as $instance) {
+?>
+<table>
+    <tr>
+        <td><?php echo $instance->post_title; ?></td><td><?php echo $instance->post_status; ?></td>
+    </tr>
+
+</table>
+<?php
+
+}
+
+?>
+</section>
+
 <?php
 get_footer();
 ?>
