7b399cdcd1eaf9fdf119356423e85d6cd81a9f79
Vinod Soba <Vinod.Soba@Addisonlee.com>
2024-07-25 14:10:19 +0100

    components added for the awards page for year 2024 styling and positioning for mobile and max width 1000px

--- wp-content/themes/addlee/templates/component-parts/component-awards.php
+++ wp-content/themes/addlee/templates/component-parts/component-awards.php
@@ -2,6 +2,7 @@
 <?php while(have_rows('awards_list')) : the_row();
     $header_light = get_sub_field('header_light');
     $columns = get_sub_field('column_count');
+    $flex_column = get_sub_field('flex_column');
 ?>
     <div class="header-light--title">
         <h1><?php echo $header_light; ?></h1>
@@ -11,12 +12,10 @@
 <?php if($columns == 12 ) : ?>
     <div class="awards-container--wrapper__single-item">
 <?php else : ?>
-    <div class="awards-container--wrapper">
+    <div class="awards-container--wrapper <?php echo $flex_column; ?>">
 <?php endif; ?>
 <?php while( have_rows('awards_list') ) : the_row();
-    
    
-
     if($columns == 4) :
         $div_class = 'col-xs-12 col-md-3';
     elseif($columns == 6) : 
@@ -29,7 +28,7 @@
     ?>
     <?php if(have_rows('fields') ) : ?>
         
-            <div class="row" style="margin:0">
+        <div class="row" style="margin:0">
 
         <?php while(have_rows('fields') ) : the_row(); ?>
 
@@ -43,15 +42,14 @@
 
             <?php else : ?>
                 
-            <div class="item-card <?php echo $div_class; ?>">
-                <?php $awards_title = get_sub_field('awards_title'); $awards_image = get_sub_field('awards_image'); ?>
-                <img src="<?php echo $awards_image; ?>">
+                <div class="item-card <?php echo $div_class; ?>">
+                    <?php $awards_title = get_sub_field('awards_title'); $awards_image = get_sub_field('awards_image'); ?>
+                    <img src="<?php echo $awards_image; ?>">
                     <div class="item-card-paragraph">
                         <p style="font-size: 16px; font-style: normal; font-weight: 400; text-transform: inherit; padding: 0px"><?php echo $awards_title;?></p>
-                    </div>
-                    
-            </div>  
-            
+                    </div>                    
+                </div> 
+         
             <?php endif; ?>
 
         <?php endwhile; ?>
